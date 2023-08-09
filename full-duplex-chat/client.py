a4d3fedbc6f473e5a9e6473c293dc738898d232f
davidinmichael <davidinmichael@gmail.com>
2023-08-09 15:39:45 +0100

    I added the full deplex program

---
+++ full-duplex-chat/client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+def send_message(client_socket):
+    while True:
+        message = input("Enter Message: ")
+        client_socket.sendall(message.encode())
+
+def receive_message(client_socket):
+    while True:
+        client_message = client_socket.recv(1024).decode()
+        print("Client: ", client_message)
+
+HOST = ""
+PORT = 9999
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+send_thread = threading.Thread(target=send_message, args=(s,))
+recv_thread = threading.Thread(target=receive_message, args=(s,))
+send_thread.start()
+recv_thread.start()
---
+++ full-duplex-chat/server.py
@@ -0,0 +1,31 @@
+from concurrent.futures import thread
+import socket
+import threading
+
+HOST = ""
+PORT = 9999
+
+def send_message(client_socket):
+    while True:
+        message = input("Enter Message: ")
+        client_socket.sendall(message.encode())
+
+def receive_message(client_socket):
+    while True:
+        client_message = client_socket.recv(1024).decode()
+        print("Client: ", client_message)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+s.bind((HOST, PORT))
+
+s.listen(5)
+print ('Listening on port', PORT )
+
+while True:
+    conn, addr = s.accept()
+    print(f"Connection to {addr} Established Successfully")
+    send_thread = threading.Thread(target=send_message, args=(conn,))
+    recv_thread = threading.Thread(target=receive_message, args=(conn,))
+    send_thread.start()
+    recv_thread.start()
