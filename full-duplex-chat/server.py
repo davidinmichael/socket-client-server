27ab2d684335b2db5e134b333d9da7c1e81edd92
davidinmichael <davidinmichael@gmail.com>
2023-08-10 13:08:39 +0100

    Other clients can see other client's messages now

--- full-duplex-chat/server.py
+++ full-duplex-chat/server.py
@@ -17,10 +17,13 @@ def send_message(client_sockets):
         for sock in client_sockets:
             sock.sendall(message.encode())
 
-def receive_message(client_socket):
+def receive_message(client_socket, client_sockets):
     while True:
         client_message = client_socket.recv(1024).decode()
         print("\nClient: ", client_message)
+
+        for sock in client_sockets:
+            sock.sendall(client_message.encode())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
@@ -34,6 +37,6 @@ while True:
     print(f"Connection to {addr} Established Successfully")
     handle_client(conn)
     send_thread = threading.Thread(target=send_message, args=(client_sockets,))
-    recv_thread = threading.Thread(target=receive_message, args=(conn,))
+    recv_thread = threading.Thread(target=receive_message, args=(conn, client_sockets))
     send_thread.start()
     recv_thread.start()
